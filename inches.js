1d878bba1f1dc49d6250639b8e6763fc813f45ec
jesminjes <mstjesmina56@gmail.com>
2023-04-13 22:38:39 +0600

    inches.js

---
+++ inches.js
@@ -0,0 +1,21 @@
+// const myInches = 12;
+// const myFeet = myInches /12;
+// console.log(myFeet);
+
+// const dataInches = 144;
+// const dadaFeet = dataInches / 12;
+// console.log('data feet', dadaFeet);
+
+// const dadiInches = 60;
+// const dadiFeet = dadiInches / 12;
+// console.log('dadiFeet', dadaFeet)
+
+function inchToFeet(inches){
+   const feet = inches / 12;
+   return feet;
+}
+
+const dadaFeet = inchToFeet(dadaInches);
+console.log(dadaFeet);
+const nanaInches = 168;
+const nanaFeet = inchToFeet(nanaFeet);
