c5732aac6d7c2ebfcd1b593abff3c5a7d89c2380
jesminjes <mstjesmina56@gmail.com>
2023-04-16 23:38:02 +0600

    iseven

--- inches.js
+++ inches.js
@@ -3,7 +3,7 @@ const myFeet = myInches /12;
 console.log(myFeet);
 
 const dataInches = 144;
-const dadaFeet = dataInches / 12;
+const dataFeet = dataInches / 12;
 console.log('data feet', dadaFeet);
 
 const dadiInches = 60;
