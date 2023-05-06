6df9bf958c4d1fbec37f6e5048e74a8bb51cddc9
jesminjes <mstjesmina56@gmail.com>
2023-05-06 20:47:30 +0600

    findodd

---
+++ findoddsum.js
@@ -0,0 +1,10 @@
+function getSumofArray(numbers){
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index]
+        console.log(index, element);
+    }
+}
+
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+getSumofArray(myNumbers);
